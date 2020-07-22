db7fc5bbc7cbd8e62489077d5639d804f2c1760a
Phillip Merriman <phillipmerriman@gmail.com>
2020-07-22 17:00:06 -0500

    initial greybox wireframe
    wireframe of quiz app

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+if you cant say pseudocode step in code, keep pseudocoding deeper.
+if you find yourself writing the same code over and over again, that could probably be a function
+
+start quiz
+click start button
+load quiz
+grab first question and choices
+display question and choices
+choose answer
+submit answer
+check answer
+correct/incorrect answer display
+grab next question and choices
+display next question and choices
+repeat till done
+display stats and replay button
+*/
